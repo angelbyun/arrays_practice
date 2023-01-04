4a01ac13c197b0a7801ae2db24e484d88b5e492b
Angel Byun <abyun22@turing.edu>
2023-01-04 16:40:03 -0700

    Add strings, integers, floats, and booleans

---
+++ arrays.rb
@@ -0,0 +1,28 @@
+# Array of Strings
+
+name = "Angel Byun"
+pet = "Harvey"
+sibling = "Christopher"
+place = "Denver"
+
+# Array of Integers
+
+age = 33
+year_of_birth = 1989
+number_of_pets = 1
+number_of_siblings = 1
+
+# Array of Floats
+
+cost_of_tuition = 20000.00
+dog_food = 50.00
+pet_insurance = 35.00
+car_insurance = 100.00
+
+# Array of Booleans
+
+paid_deposit = true
+paid_tuition = false
+loan_disbursed = false
+loan_approval = true
+
